c83cbdf0a4434e1727ee811b83352596b747af65
Ali Emre Çakmakoğlu <aliemrecakmakoglu@gmail.com>
2020-10-20 17:20:26 +0300

    Framework extra bundle installed

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
